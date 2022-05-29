0688a6ca0b2ecd1e1eb102d2dc6869771510cd70
Luciana <lucianamelisa@gmail.com>
2022-05-29 01:17:19 -0300

    ADD: Data type conversion & arrays

---
+++ Práctica extra/Conversión de tipos de datos/Conversión de tipos de datos/Program.cs
@@ -0,0 +1,79 @@
+﻿/**********Conversión de tipos de datos entre primitivos. Casting implícito y explícito************/
+//Método para convertir datos, que son compatibles entre sí, en otro de diferente alcance.
+
+/**************************************Casting implícito*******************************************/
+//de int a double
+int numero1 = 9;
+double numero2 = numero1;
+
+//de int a long
+int numero3 = 9;
+double numero4 = numero3;
+
+//de int a float
+int numero5 = 9;
+double numero6 = numero5;
+
+//de int a decimal
+int numero7 = 9;
+decimal numero8 = numero7;
+
+//de float a double
+float numero9 = 9.45f;
+double numero10 = numero9;
+
+/**************************************Casting explícito*******************************************/
+//de float a decimal
+float numero11 = 9.45f;
+decimal numero12 = (decimal)numero11;
+
+//de float a int
+float numero13 = 9.45f;
+int numero14 = (int)numero13;
+
+//de double a decimal
+double numero15 = 9.45d;
+decimal numero16 = (decimal)numero15;
+
+//de double a int
+double numero17 = 9.45d;
+int numero18 = (int)numero17;
+
+//de double a float
+double numero19 = 9.45d;
+float numero20 = (float)numero19;
+
+//de decimal a double
+decimal numero21 = 9.45m;
+double numero22 = (double)numero21;
+
+//de decimal a float
+decimal numero23 = 9.45m;
+float numero24 = (float)numero23;
+
+//de decimal a int
+decimal numero25 = 9.45m;
+int numero26 = (int)numero25;
+
+/***********************Método para convertir tipos de datos que no son compatibles***************/
+//Método .Parse(): 
+
+//de texto a int
+Console.WriteLine("Ingrese un número");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine(num1);
+
+//de texto a float
+Console.WriteLine("Ingrese un número");
+float num2 = float.Parse(Console.ReadLine());
+Console.WriteLine(num2);
+
+//de texto a double
+Console.WriteLine("Ingrese un número");
+double num3 = double.Parse(Console.ReadLine());
+Console.WriteLine(num3);
+
+//de texto a decimal
+Console.WriteLine("Ingrese un número");
+decimal num4 = decimal.Parse(Console.ReadLine());
+Console.WriteLine(num4);
--- Práctica extra/MatricesUnidimensionales/MatricesUnidimensionales/Program.cs
+++ Práctica extra/MatricesUnidimensionales/MatricesUnidimensionales/Program.cs
@@ -1,7 +1,7 @@
 ﻿/**********************Matrices en C#***********************/
 //Guarda en una variable varios datos de un mismo tipo
 //Es mutable
-//Puede ser unidimensional (array/vector), o multidimensional
+//Puede ser unidimensional (array/vector), bidimensionales o multidimensional
 
 /*******Declaración de Matrices unidimensional o array******/
 //Matriz Unidimensional o Array o Vector
@@ -77,4 +77,170 @@ for (int i = 0; i < numbers.Length; i++)
 {
     //Imprimo los números ingresados
     Console.WriteLine($"En el índice {i} usted ingresó {numbers[i]}");
+}
+
+
+/***********Valores por defecto en arrays/vectores**************/
+
+/*Hasta no asignarle los elementos a cada posición, cada espacio en memoria guarda
+un valor por defecto dependiendo del tipo de dato*/
+int[] myIntArray;
+myIntArray = new int[5];
+Console.WriteLine(myIntArray[3]); //por defecto imprime 0
+
+float[] myFloatArray;
+myFloatArray = new float[5];
+Console.WriteLine(myFloatArray[3]); //por defecto imprime 0
+
+string[] myStringArray;
+myStringArray = new string[5];
+Console.WriteLine(myStringArray[3]); //por defecto imprime un espacio vacío
+
+char[] myCharArray;
+myCharArray = new char[5];
+Console.WriteLine(myCharArray[3]); //por defecto imprime un espacio vacío
+
+double[] myDoubleArray;
+myDoubleArray = new double[5];
+Console.WriteLine(myDoubleArray[3]); //por defecto imprime 0
+
+//string[] myStringArray;
+//myStringArray = new string[5];
+myStringArray = new string[5] { "Ana", "Martina", "", "Sofia", "Lucia" };
+Console.WriteLine(myStringArray[2]);
+Console.WriteLine(myStringArray[3]);
+
+//int[] myIntArray;
+//myIntArray = new int[5];
+myIntArray[0] = 1;
+myIntArray[1] = 2;
+//myIntArray[2] //lo dejo vacío
+myIntArray[3] = 4;
+myIntArray[4] = 5;
+Console.WriteLine(myIntArray[2]);
+Console.WriteLine(myIntArray[3]);
+Console.WriteLine();
+
+
+/***********Arrays/vectores - Uso del método .Length***********************/
+/
+/*Creo un array de 5 elementos con inputs por pantalla, imprimo los valores 
+ y calculo el promedio del array*/
+Console.WriteLine("===============================================");
+Console.WriteLine("Array de 5 elementos");
+Console.WriteLine("===============================================");
+
+int[] numerosArray = new int[5];
+int sumaArray = 0;
+float promedio = 0;
+
+for (int i = 0; i < numerosArray.Length; i++)
+{
+    Console.WriteLine($"Ingrese el valor para el {i + 1}° elemento del array");
+    numerosArray[i] = int.Parse(Console.ReadLine());
+    sumaArray += numerosArray[i];
+}
+Console.WriteLine("===============================================");
+
+Console.WriteLine("El vector tiene los siguientes valores:");
+for (int i = 0; i < numerosArray.Length; i++)
+{
+    Console.WriteLine(numerosArray[i]);
+}
+Console.WriteLine("================================================");
+
+promedio = (float)sumaArray / numerosArray.Length;
+Console.WriteLine($"El promedio de los valores del array es {promedio}");
+
+Console.WriteLine();
+
+
+/*Similar que el anterior, sólo que el tamaño del array lo define el usuario por pantalla, 
+imprimo los valores y calculo el promedio del array*/
+Console.WriteLine("===============================================");
+Console.WriteLine("Array de n elementos, definido por el usuario");
+Console.WriteLine("===============================================");
+
+Console.WriteLine("Ingrese la cantidad de elementos del array");
+int elementosArray = int.Parse(Console.ReadLine());
+
+int[] numerosArray_ = new int[elementosArray];
+int sumaArray_ = 0;
+float promedio_ = 0;
+
+for (int i = 0; i < numerosArray_.Length; i++)
+{
+    Console.WriteLine($"Ingrese el valor para el {i + 1}° elemento del array");
+    numerosArray_[i] = int.Parse(Console.ReadLine());
+    sumaArray_ += numerosArray_[i];
+}
+Console.WriteLine("===============================================");
+
+Console.WriteLine("El vector tiene los siguientes valores:");
+for (int i = 0; i < numerosArray_.Length; i++)
+{
+    Console.WriteLine(numerosArray_[i]);
+}
+Console.WriteLine("================================================");
+
+promedio_ = (float)sumaArray_ / numerosArray_.Length;
+Console.WriteLine($"El promedio de los valores del array es {promedio_}");
+
+Console.WriteLine();
+
+
+/***********Arrays/vectores de tipo implícito***************************/
+
+/*se declara sin especificar tipo de dato, ni tamaño del array. Se debe asignar 
+los elementos al momento de declararlo, por lo que el array implícito debe ser 
+inicializado siempre*/
+
+//Array implícito de números enteros
+var arrayImplicitoInt = new[] { 2, 4, 5, 7 };
+
+//Para recorrer el array implícito
+Console.WriteLine("Elementos en array implícito de integers:");
+for (int i = 0; i < arrayImplicitoInt.Length; i++)
+{
+    Console.WriteLine($"El {i + 1}° valor es {arrayImplicitoInt[i]}");
+}
+
+
+//Array implícito de números doubles
+var arrayImplicitoDouble = new[] { 2.53, 4.53, 5.54, 7.55 };
+
+//Para recorrer el array implícito
+Console.WriteLine("Elementos en array implícito de doubles:");
+for (int i = 0; i < arrayImplicitoDouble.Length; i++)
+{
+    Console.WriteLine($"El {i + 1}° valor es {arrayImplicitoDouble[i]}");
+}
+
+
+//Array implícito de números doubles con enteros
+//Con que haya 1 sólo double en un array de integers, ese o convierte en un array de doubles
+var arrayImplicitoDoubleInt = new[] { 2, 4.53, 5, 7 };
+
+//Para recorrer el array implícito
+Console.WriteLine("Elementos en array implícito de doubles y enteros:");
+for (int i = 0; i < arrayImplicitoDoubleInt.Length; i++)
+{
+    Console.WriteLine($"El {i + 1}° valor es {arrayImplicitoDoubleInt[i]}");
+}
+
+
+//Array implícito de strings
+var arrayImplicitoStrings = new[] { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes" };
+
+//Para recorrer el array implícito con for
+Console.WriteLine("Elementos en array implícito de strings:");
+for (int i = 0; i < arrayImplicitoStrings.Length; i++)
+{
+    Console.WriteLine($"El {i + 1}° valor es {arrayImplicitoStrings[i]}");
+}
+
+//foreach para recorrer arrays implícitos. Menos uso de código para recorrerlo
+foreach (string var in arrayImplicitoStrings)
+{
+    Console.WriteLine(var);
 }
